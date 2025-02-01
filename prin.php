9be53bb94a587e2d0459ae0dbca01cafa08b2a5c
Locolexe <robots529j@gmail.com>
2025-01-31 19:40:01 -0600

    No message

--- prin.php
+++ prin.php
@@ -2,13 +2,11 @@
     <body>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Recibir datos del formulario HTML
         $corre = $_POST['Corr1'];
         $pass = $_POST['Pass1'];
 
         // Validar y procesar los datos recibidos
         if (!empty($corre) && !empty($pass)) {
-            // Aquí puedes agregar el código para procesar los datos, como guardarlos en una base de datos
             echo "Nombre: " . htmlspecialchars($corre) . "<br>";
             echo "Email: " . htmlspecialchars($pass) . "<br>";
         } else {
