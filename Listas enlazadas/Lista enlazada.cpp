c094989d48e3f32ce70cfdf966e2fe4bf172c802
PabloS <117939002+Locolexe@users.noreply.github.com>
2025-02-04 16:04:19 -0600

    Update Lista enlazada.cpp

--- Listas enlazadas/Lista enlazada.cpp
+++ Listas enlazadas/Lista enlazada.cpp
@@ -23,7 +23,7 @@ struct ListaEnlazadaEstatica {
             nodos[i].anterior = i-1; // Inicializamos la lista de nodos libres (Solo dobles enlazada)
         }
         nodos[9].siguiente = -1; // El último nodo libre no tiene siguiente
-        nodos[9].anterior = -1; // El último nodo libre no tiene anterior (Solo dobles enlazada)
+        nodos[0].anterior = -1; // El último nodo libre no tiene anterior (Solo dobles enlazada)
     }
 
     // Función para insertar un dato en la lista
